562b224b30b2a54894ee33649c2711ce8c63e235
Oleksandr Baglai <oleksandr_baglai@epam.com>
2021-12-25 02:42:50 +0200

    [engine][events][scores][games][all] Refactoring. Used Void if no value.

--- src/main/java/com/codenjoy/dojo/chess/service/GameSettings.java
+++ src/main/java/com/codenjoy/dojo/chess/service/GameSettings.java
@@ -138,7 +138,7 @@ public class GameSettings extends SettingsImpl implements SettingsReader<GameSet
         }
     }
 
-    public Calculator<Integer> calculator() {
+    public Calculator<Void> calculator() {
         return new Calculator<>(new Scores(this));
     }
 }
